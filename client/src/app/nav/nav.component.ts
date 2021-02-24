309fbb30ff69fc06fea1c0f4f1cbf6bb5dc3a4f4
Danieltje <43807304+Danieltje@users.noreply.github.com>
2021-02-24 12:35:20 +0100

    almost finished section 5

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,26 +12,28 @@ export class NavComponent implements OnInit {
   // create a class property to store what the users enters into the form
   // model property of type any and initialise this to an empty object
   model: any = {}
-  loggedIn: boolean;
 
   // inject our account.service.ts into the nav component
-  constructor(private accountService: AccountService) { }
+  // if we want to access a service from here we need to make it public
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+    
   }
 
   login() {
     // use the account.service to actually login our user
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     } )
   }
 
   logout() {
-    this.loggedIn = false;
+
+    // here we actually initiate the logout function we made in the account.service.ts file
+    this.accountService.logout();
   }
 
 }
