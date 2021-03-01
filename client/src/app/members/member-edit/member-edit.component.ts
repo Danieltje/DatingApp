7dbc57b0d0f1b50e93f4fdda25e5a6511c9d9238
Danieltje <smit-daniel@hotmail.com>
2021-03-01 16:39:16 +0100

    begin section 10

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  member: Member;
+  user: User;
+  
+  // we need to fetch the the current user (accountservice)
+  // and with that we need to get the username, so we can get the username and go and fetch that particular member (memberservice)
+  constructor(private accountService: AccountService, private memberService: MembersService) {
+    // We need to get to get the current user. Remember that it's an Observable, so we need to get it out of there
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+   }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.memberService.getMember(this.user.username).subscribe(member => {
+      this.member = member;
+    })
+  }
+
+}
