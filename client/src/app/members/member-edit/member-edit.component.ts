e1728c403e09428c4e874276b08df3c08adc6669
Danieltje <smit-daniel@hotmail.com>
2021-03-01 17:17:58 +0100

    almost halfway section 10

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
@@ -11,14 +13,19 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+
+  // This ViewChild gives us access to the template form inside the html
+  // This means that when the view DOM changes, and a new child matches the selector, the property is updated
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
   
   // we need to fetch the the current user (accountservice)
   // and with that we need to get the username, so we can get the username and go and fetch that particular member (memberservice)
-  constructor(private accountService: AccountService, private memberService: MembersService) {
+  constructor(private accountService: AccountService, private memberService: MembersService,
+    private toastr: ToastrService) {
     // We need to get to get the current user. Remember that it's an Observable, so we need to get it out of there
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
    }
 
   ngOnInit(): void {
@@ -29,6 +36,12 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(this.user.username).subscribe(member => {
       this.member = member;
     })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm.reset(this.member);
   }
 
 }
